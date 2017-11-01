16e269ebf1c30ce7a1171c4cc3ae5487c02149be
Kunal Shroff <shroffk@bnl.gov>
2017-11-01 17:14:38 -0400

    prototype LogBuilder

--- src/main/java/edu/msu/nscl/olog/Log.java
+++ src/main/java/edu/msu/nscl/olog/Log.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -52,7 +53,13 @@ public class Log implements Serializable {
     private Set<Logbook> logbooks = new HashSet<Logbook>();
     private Set<Tag> tags = new HashSet<Tag>();
 
-    public Log(Long id, String version, String owner, String source, String description, Level level, State state,
+    // TODO add support for attachments
+
+    private Log() {
+
+    }
+
+    private Log(Long id, String version, String owner, String source, String description, Level level, State state,
             Date modifiedDate, Collection<Property> properties, Set<Logbook> logbooks, Set<Tag> tags) {
 
         super();
@@ -69,4 +76,104 @@ public class Log implements Serializable {
         this.tags = tags;
     }
 
+    public static class LogBuilder {
+
+        private String version;
+        private String owner;
+        private String source;
+        private StringBuilder description;
+
+        private Level level = Level.Info;
+        private State state = State.Active;
+
+        private Set<Property> properties = new HashSet<Property>();
+        private Set<Logbook> logbooks = new HashSet<Logbook>();
+        private Set<Tag> tags = new HashSet<Tag>();
+
+        public LogBuilder() {
+        }
+
+        public LogBuilder(String source) {
+            this.source = source;
+        }
+
+        public static LogBuilder createLog() {
+            return new LogBuilder();
+        }
+
+        public static LogBuilder createLog(String source) {
+            return new LogBuilder(source);
+        }
+
+        public LogBuilder description(String description) {
+            if (description != null)
+                this.description = new StringBuilder(description);
+            else if (description == null)
+                this.description = null;
+            return this;
+        }
+
+        public LogBuilder appendDescription(String description) {
+            if (this.description == null)
+                this.description = new StringBuilder(description);
+            else if (this.description != null)
+                this.description.append("\n").append(description);
+            return this;
+        }
+
+        public LogBuilder level(Level level) {
+            this.level = level;
+            return this;
+        }
+
+        public LogBuilder addLogbook(Logbook logbook) {
+            this.logbooks.add(logbook);
+            return this;
+        }
+
+        public LogBuilder addLogbooks(Set<Logbook> logbooks) {
+            this.logbooks.addAll(logbooks);
+            return this;
+        }
+
+        public LogBuilder setLogbooks(Set<Logbook> logbooks) {
+            this.logbooks = logbooks;
+            return this;
+        }
+
+        public LogBuilder addTag(Tag tag) {
+            this.tags.add(tag);
+            return this;
+        }
+
+        public LogBuilder addTags(Set<Tag> tags) {
+            this.tags.addAll(tags);
+            return this;
+        }
+
+        public LogBuilder setTags(Set<Tag> tags) {
+            this.tags = tags;
+            return this;
+        }
+
+        public LogBuilder addProperty(Property property) {
+            this.properties.add(property);
+            return this;
+        }
+
+        public LogBuilder addProperties(Set<Property> properties) {
+            this.properties.addAll(properties);
+            return this;
+        }
+
+        public LogBuilder setProperties(Set<Property> properties) {
+            this.properties = properties;
+            return this;
+        }
+
+        public Log build() {
+            // TODO add validation
+            return new Log();
+        }
+    }
 }
---
+++ src/test/java/edu/msu/nscl/olog/LogManagerIT.java
@@ -0,0 +1,24 @@
+package edu.msu.nscl.olog;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class LogManagerIT {
+
+    /**
+     * A basic test to create a simple tag
+     */
+    @Test
+    public void createLog() {
+      
+    }
+
+}
