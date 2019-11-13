62586100e2bc839b7d623b0c83b19e23e8166384
Kunal Shroff <kunalshroff9@gmail.com>
2019-11-13 10:19:36 -0500

    update the sequence generator to use high level rest api for elastic

--- src/main/java/gov/bnl/olog/SequenceGenerator.java
+++ src/main/java/gov/bnl/olog/SequenceGenerator.java
@@ -3,13 +3,17 @@
  */
 package gov.bnl.olog;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,29 +26,32 @@ public class SequenceGenerator
 
     public static final String seqIndex = "olog_sequence";
 
-    private static Client client;
-
     @Autowired
-    ElasticsearchOperations es;
+    @Qualifier("indexClient")
+    RestHighLevelClient indexClient;
 
-    private static SequenceGenerator instance = new SequenceGenerator();
+//    private static SequenceGenerator instance = new SequenceGenerator();
+    private static RestHighLevelClient client;
 
 
     @PostConstruct
     public void init()
     {
-        SequenceGenerator.client = es.getClient();
+        Application.logger.config("Initializing the unique sequence id generator");
+        SequenceGenerator.client = indexClient;
     }
 
     /**
      * get a new unique id from the olog_sequnce index
      * 
-     * @return
+     * @return a new unique id for a olog entry
+     * @throws IOException 
      */
-    public static long getID()
+    public static long getID() throws IOException
     {
-        IndexResponse response = client.prepareIndex("olog_sequence", "olog_sequence", "id")
-                .setSource(0, XContentType.class).get();
+        IndexResponse response = client.index(
+                new IndexRequest("olog_sequence", "olog_sequence", "id").source(0, XContentType.class),
+                RequestOptions.DEFAULT);
         return response.getVersion();
     }
 
