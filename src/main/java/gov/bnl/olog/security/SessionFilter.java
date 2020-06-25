9af3279d8581718814e972a66213684d24e8efc2
Georg Weiss <georg.weiss@ess.eu>
2020-06-25 20:40:14 +0200

    Issue_6 update to .gitignore as it excluded required files

---
+++ src/main/java/gov/bnl/olog/security/SessionFilter.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2020 European Spallation Source ERIC.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package gov.bnl.olog.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.bnl.olog.WebSecurityConfig;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * Filter that will take care of authenticating requests that come with either a session cookie or
+ * a basic authentication header.
+ * <p>
+ * This class should not be instantiated as a bean in the application configuration. If it is, the
+ * <code>doFilter()</code> method will be called for each endpoint URI, effectively defeating the purpose of the
+ * configuration of ignored URI patterns set up in the Spring Security context, see
+ * {@link WebSecurityConfig#configure(WebSecurity)}.
+ */
+public class SessionFilter extends GenericFilterBean {
+
+    private AuthenticationManager authenticationManager;
+    private FindByIndexNameSessionRepository sessionRepository;
+    private ObjectMapper objectMapper;
+
+    public SessionFilter(AuthenticationManager authenticationManager, FindByIndexNameSessionRepository sessionRepository) {
+        this.authenticationManager = authenticationManager;
+        this.sessionRepository = sessionRepository;
+        objectMapper = new ObjectMapper();
+    }
+
+    /**
+     * The request is authenticated as follows:
+     * <ol>
+     *     <li>If the request contains a cookie named SESSION, the session repository is queried to check if
+     *     a session associated with the cookie value exists and is not expired. A non-expired session will be
+     *     used to set the {@link Authentication} in the security context.</li>
+     *     <li>If there is no SESSION cookie, or if the session associated with such a cookie is expired,
+     *     the request is checked for a Authorization header. If it exists, its decoded username and password values
+     *     are provided to the {@link AuthenticationManager} authentication. A successful authentication will then return
+     *     the {@link Authentication} object that will passed to the security context.</li>
+     *     <li>If none the previous steps is able to authenticate the request, <code>null</code> will be passed
+     *     to the security context, i.e. request is not authenticated.</li>
+     * </ol>
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        Authentication authentication = getAuthenticationFromCookie(httpServletRequest);
+        if (authentication != null) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            String basicAuthenticationHeader = httpServletRequest.getHeader("Authorization");
+            String[] usernameAndPassword = getUsernameAndPassword(basicAuthenticationHeader);
+            if (usernameAndPassword == null) {
+                SecurityContextHolder.getContext().setAuthentication(null);
+            } else {
+                authentication = new UsernamePasswordAuthenticationToken(usernameAndPassword[0],
+                        usernameAndPassword[1]);
+                try {
+                    authentication = authenticationManager.authenticate(authentication);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } catch (AuthenticationException e) {
+                    Logger.getLogger(SessionFilter.class.getName())
+                            .log(Level.FINE, String.format("User %s not authenticated through authorization header",
+                                    usernameAndPassword[0]));
+                }
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    protected String[] getUsernameAndPassword(String authorization) {
+        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
+            // Authorization: Basic base64credentials
+            String base64Credentials = authorization.substring("Basic".length()).trim();
+            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+            // credentials = username:password
+            return credentials.split(":", 2);
+        }
+        return null;
+    }
+
+    /**
+     * Returns a {@link Authentication} object if the <code>httpServletRequest</code> contains
+     * a cookie associated with a non-expired session.
+     *
+     * @param httpServletRequest
+     * @return
+     */
+    protected Authentication getAuthenticationFromCookie(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if (cookies == null || cookies.length == 0) {
+            return null;
+        }
+        for (Cookie cookie : cookies) {
+            if ("SESSION".equals(cookie.getName())) {
+                Session session = sessionRepository.findById(cookie.getValue());
+                if (session == null) { // No need to check expired, repository does it.
+                    // Do not break. The request may contain multiple cookies named SESSION.
+                    continue;
+                }
+                // Update last access time, repository does not do it automatically.
+                session.setLastAccessedTime(Instant.now());
+                sessionRepository.save(session);
+                List<String> roles = session.getAttribute(WebSecurityConfig.ROLES_ATTRIBUTE_NAME);
+                String userName = session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME);
+                List<GrantedAuthority> grantedAuthorities =
+                        roles.stream().map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
+                return new UsernamePasswordAuthenticationToken(userName,
+                        null, grantedAuthorities);
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/gov/bnl/olog/security/SessionFilterTest.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2020 European Spallation Source ERIC.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package gov.bnl.olog.security;
+
+import gov.bnl.olog.WebSecurityConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.MapSession;
+import org.springframework.session.Session;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@ContextHierarchy({@ContextConfiguration(classes = {SessionFilterTestConfig.class})})
+public class SessionFilterTest {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private FindByIndexNameSessionRepository sessionRepository;
+
+    private SessionFilter sessionFilter;
+    private HttpServletResponse httpServletResponse;
+    private FilterChain filterChain;
+    private Cookie[] cookies;
+
+    @Before
+    public void init() {
+        sessionFilter = new SessionFilter(authenticationManager, sessionRepository);
+        httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        filterChain = Mockito.mock(FilterChain.class);
+        cookies = new Cookie[]{new Cookie("SESSION", "abc")};
+    }
+
+    @Test
+    public void testGetAuthorizationFromCookie() {
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(null);
+
+        Authentication authentication = sessionFilter.getAuthenticationFromCookie(httpServletRequest);
+        assertNull(authentication);
+
+        reset(httpServletRequest);
+        when(httpServletRequest.getCookies()).thenReturn(new Cookie[]{});
+        authentication = sessionFilter.getAuthenticationFromCookie(httpServletRequest);
+        assertNull(authentication);
+
+        reset(httpServletRequest);
+        Cookie[] cookies = new Cookie[]{new Cookie(WebSecurityConfig.SESSION_COOKIE_NAME, "b"),
+                new Cookie(WebSecurityConfig.SESSION_COOKIE_NAME, "a")};
+        when(httpServletRequest.getCookies()).thenReturn(cookies);
+        when(sessionRepository.findById("a")).thenReturn(null);
+        authentication = sessionFilter.getAuthenticationFromCookie(httpServletRequest);
+        assertNull(authentication);
+
+        reset(httpServletRequest);
+        when(httpServletRequest.getCookies()).thenReturn(cookies);
+        Session session = new MapSession();
+        session.setAttribute(WebSecurityConfig.ROLES_ATTRIBUTE_NAME, Arrays.asList("role1"));
+        session.setAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, "user");
+        when(sessionRepository.findById("a")).thenReturn(session);
+        authentication = sessionFilter.getAuthenticationFromCookie(httpServletRequest);
+        assertNotNull(authentication);
+    }
+
+    @Test
+    public void testGetUsernameAndPasswordFromAuthorizationHeader(){
+        assertNull(sessionFilter.getUsernameAndPassword(null));
+        assertNull(sessionFilter.getUsernameAndPassword("Does not start with Basic"));
+
+        String[] usernameAndPassword = sessionFilter.getUsernameAndPassword("Basic YWRtaW46YWRtaW5QYXNz");
+        assertEquals("admin", usernameAndPassword[0]);
+        assertEquals("adminPass", usernameAndPassword[1]);
+    }
+    
+    @Test
+    public void testFilterWithoutCookieOrAuthenticationHeader() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(null);
+        when(httpServletRequest.getHeader("Authorization")).thenReturn(null);
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+
+        when(httpServletRequest.getCookies()).thenReturn(new Cookie[]{});
+        when(httpServletRequest.getHeader("Authorization")).thenReturn(null);
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    @Test
+    public void testFilterWithNullSession() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(cookies);
+        when(sessionRepository.findById("abc")).thenReturn(null);
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    @Test
+    public void testFilterWithValidSession() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(cookies);
+        Session session = mock(Session.class);
+        when(session.isExpired()).thenReturn(false);
+        when(session.getAttribute(WebSecurityConfig.ROLES_ATTRIBUTE_NAME)).thenReturn(Arrays.asList("ROLE_ADMIN"));
+        when(session.getAttribute(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME)).thenReturn("ADMIN");
+        when(sessionRepository.findById("abc")).thenReturn(session);
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assertNotNull(authentication);
+        assertEquals("ADMIN", authentication.getName());
+        assertEquals("ROLE_ADMIN", authentication.getAuthorities().iterator().next().getAuthority());
+    }
+
+    @Test
+    public void testFilterWithWrongAuthorizationHeader() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(null);
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("wrong header value");
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    @Test
+    public void testFilterWithInvalidAuthorizationHeader() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(null);
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("Basic YWRtaW46YWRtaW5QYXNz");
+        doThrow(new BadCredentialsException("bad")).when(authenticationManager).authenticate(any(Authentication.class));
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    @Test
+    public void testFilterWithValidAuthorizationHeader() throws Exception{
+        HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
+        when(httpServletRequest.getCookies()).thenReturn(null);
+        when(httpServletRequest.getHeader("Authorization")).thenReturn("Basic YWRtaW46YWRtaW5QYXNz");
+        Authentication authentication = new UsernamePasswordAuthenticationToken("ADMIN",
+                "ADMIN_PASS");
+        when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(authentication);
+        sessionFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+        authentication = SecurityContextHolder.getContext().getAuthentication();
+        assertNotNull(authentication);
+        assertEquals("ADMIN", authentication.getName());
+    }
+}
