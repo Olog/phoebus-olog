6aa875ce171ed86938e7c0aac03431c1059929c8
Kunal Shroff <kunalshroff9@gmail.com>
2023-05-22 09:48:51 -0400

    #163 add explicit flush calls in the data migration scripts

--- scripts/update-olog-logs-index.sh
+++ scripts/update-olog-logs-index.sh
@@ -13,12 +13,20 @@ echo "Copying data to temporary index"
 curl -XPOST "http://localhost:9200/_reindex"  -H 'Content-Type: application/json' -d '{"source":{"index": "olog_logs"}, "dest":{"index":"olog_logs_tmp"}}'
 echo
 
+echo "Flushing data to temporary index"
+curl -XPOST "http://localhost:9200/olog_logs_tmp/_flush"
+echo
+
 echo "Delete original index"
 curl -XDELETE "http://localhost:9200/olog_logs"
 echo
 
 echo "Re-creating original index"
 curl -XPUT "http://localhost:9200/olog_logs"  -H 'Content-Type: application/json' --data @$1
+echo
+
+echo "Flushing data to temporary index"
+curl -XPOST "http://localhost:9200/olog_logs/_flush"
 echo
 
 echo "Copying data to original index"
