3cf21c2559f5b4608aeceefe861b7ec65939a83e
Giovanni Lorenzo Napoleoni <giovanni.lorenzo.napoleoni@lnf.infn.it>
2025-05-30 09:32:35 +0200

    handle file attachment on edit

--- src/main/java/org/phoebus/olog/LogResource.java
+++ src/main/java/org/phoebus/olog/LogResource.java
@@ -363,7 +363,8 @@ public class LogResource {
     @PostMapping("/{logId}")
     public Log updateLog(@PathVariable String logId,
                          @RequestParam(value = "markup", required = false) String markup,
-                         @RequestBody Log log,
+                         @RequestPart("logEntry") Log log,
+                         @RequestPart(value = "files", required = false) MultipartFile[] files,
                          @AuthenticationPrincipal Principal principal) {
 
         // In case a client sends a log record where the id does not match the path variable, return HTTP 400 (bad request)
@@ -396,6 +397,42 @@ public class LogResource {
             persistedLog.setLogbooks(log.getLogbooks());
             persistedLog.setTitle(log.getTitle());
             persistedLog = cleanMarkup(markup, persistedLog);
+            
+            // handle attachments
+            Set<Attachment> existingAttachments = persistedLog.getAttachments();
+            Set<Attachment> newAttachments = log.getAttachments();
+
+            // Remove attachments that are no longer present
+            existingAttachments.removeIf(existing ->
+                    newAttachments.stream().noneMatch(newAtt ->
+                            newAtt.getId() != null && newAtt.getId().equals(existing.getId())));
+
+            // Add or update attachments
+            for (Attachment newAttachment : newAttachments) {
+                Optional<Attachment> existingAttachment = existingAttachments.stream()
+                        .filter(a -> a.getId().equals(newAttachment.getId()))
+                        .findFirst();
+                if (existingAttachment.isPresent()) {
+                    existingAttachment.get().setFilename(newAttachment.getFilename());
+                    existingAttachment.get().setFileMetadataDescription(newAttachment.getFileMetadataDescription());
+                } else {
+                    // New attachment to save
+                    MultipartFile matchingFile = null;
+                    if (files != null) {
+                        for (MultipartFile file : files) {
+                            if (Objects.equals(file.getOriginalFilename(), newAttachment.getFilename())) {
+                                matchingFile = file;
+                                break;
+                            }
+                        }
+                    }
+
+                    Attachment attachment = new Attachment(newAttachment.getId(), matchingFile, newAttachment.getFilename(), newAttachment.getFileMetadataDescription());
+                    Attachment savedAttachment = attachmentRepository.save(attachment);
+                    existingAttachments.add(savedAttachment);
+                }
+            }
+            persistedLog.setAttachments(existingAttachments);
 
             return logRepository.update(persistedLog);
         } else {
